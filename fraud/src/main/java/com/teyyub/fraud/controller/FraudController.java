91a6f8959325e667f4d7f75d07224fe731d35310
teyyubismayil <teyyub.ismayilov@mail.ru>
2022-06-12 14:22:21 +0400

    add eureka server

--- fraud/src/main/java/com/teyyub/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/teyyub/fraud/controller/FraudController.java
@@ -3,11 +3,13 @@ package com.teyyub.fraud.controller;
 import com.teyyub.fraud.model.FraudCheckResponse;
 import com.teyyub.fraud.service.FraudCheckService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/fraud-check")
@@ -17,6 +19,7 @@ public class FraudController {
 
     @PostMapping("/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") long customerId) {
+        log.info("Fraud check: {}", customerId);
         return new FraudCheckResponse(
                 fraudCheckService.isFraudulentCustomer(customerId)
         );
