5fd6423c09d3152d365fbbe1045b5249876366b3
teyyubismayil <teyyub.ismayilov@mail.ru>
2022-06-12 19:02:58 +0400

    cliens module

---
+++ clients/src/main/java/com/teyyub/clients/fraud/FraudCheckResponse.java
@@ -0,0 +1,6 @@
+package com.teyyub.clients.fraud;
+
+public record FraudCheckResponse(
+        boolean response
+) {
+}
--- fraud/src/main/java/com/teyyub/fraud/controller/FraudController.java
+++ fraud/src/main/java/com/teyyub/fraud/controller/FraudController.java
@@ -4,10 +4,7 @@ import com.teyyub.fraud.model.FraudCheckResponse;
 import com.teyyub.fraud.service.FraudCheckService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -17,7 +14,7 @@ public class FraudController {
 
     private final FraudCheckService fraudCheckService;
 
-    @PostMapping("/{customerId}")
+    @GetMapping("/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable("customerId") long customerId) {
         log.info("Fraud check: {}", customerId);
         return new FraudCheckResponse(
